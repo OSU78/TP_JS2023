e95e8f4b8e6c6d244f6ea78dc19e4ee280218b12
richflor <fld.richard@gmail.com>
2023-02-03 12:14:58 +0100

    fix commit

--- PROJET-bonus/post.js
+++ PROJET-bonus/post.js
@@ -1,106 +1,106 @@
 let currentUserId = null;
-const postsContainer = document.querySelector('#posts');
-const templatePost = document.querySelector('#templatePost');
+const postsContainer = document.querySelector("#posts");
+const templatePost = document.querySelector("#templatePost");
 const avgComContainer = document.querySelector("#avgNumberComments");
 const topWordsPostsContainer = document.querySelector("#topWordsPosts");
 import { userSelect } from "./start.js";
 import { get } from "./start.js";
 
 async function displayPostsUser(userId) {
-    const posts = await get("/posts", ["userId", userId]);
-    const container = document.createElement("div");
-    postsContainer.append(container);
+  const posts = await get("/posts", ["userId", userId]);
+  const container = document.createElement("div");
+  postsContainer.append(container);
 
-    let totalComments = 0;
-    let aggregateWords = "";
+  let totalComments = 0;
+  let aggregateWords = "";
 
-    for (const post of posts) {
-        const elem = await createPostElem(post);
-        container.append(elem);
-        totalComments += Number(elem.dataset.numberComs);
-        aggregateWords += `${elem.children[1].textContent} `;
-    }
+  for (const post of posts) {
+    const elem = await createPostElem(post);
+    container.append(elem);
+    totalComments += Number(elem.dataset.numberComs);
+    aggregateWords += `${elem.children[1].textContent} `;
+  }
 
-    const avgCommentsPerPost = totalComments/posts.length;
-    // display the average number of comments per post
-    avgComContainer.textContent = `Nombre moyen de commentaires par post : ${avgCommentsPerPost}`;
+  const avgCommentsPerPost = totalComments / posts.length;
+  // display the average number of comments per post
+  avgComContainer.textContent = `Nombre moyen de commentaires par post : ${avgCommentsPerPost}`;
 
-    const array = await countWords(aggregateWords);
-    topWordsPostsContainer.insertAdjacentHTML("beforeend", topWords(array));
-    //console.log(array)
+  const array = await countWords(aggregateWords);
+  topWordsPostsContainer.insertAdjacentHTML("beforeend", topWords(array));
+  //console.log(array)
 }
 
 async function createPostElem(data) {
-    const post = document.createElement("div");
+  const post = document.createElement("div");
 
-    const numberComs = await getNumberComments(data.id);
+  const numberComs = await getNumberComments(data.id);
 
-    post.dataset.id= data.id;
-    post.dataset.userId = data.userId;
-    post.dataset.numberComs = numberComs;
+  post.dataset.id = data.id;
+  post.dataset.userId = data.userId;
+  post.dataset.numberComs = numberComs;
 
-    post.classList.add("post");
-    post.append(templatePost.content.cloneNode(true))
+  post.classList.add("post");
+  post.append(templatePost.content.cloneNode(true));
 
-    post.children[0].textContent = `Titre: ${data.title}`;
-    post.children[1].textContent = data.body;
-    post.children[2].textContent = `Nombre de commentaires: ${numberComs}`
+  post.children[0].textContent = `Titre: ${data.title}`;
+  post.children[1].textContent = data.body;
+  post.children[2].textContent = `Nombre de commentaires: ${numberComs}`;
 
-    return post;
+  return post;
 }
 
 async function getNumberComments(postId) {
-    const comments = await get("/comments", ["postId", postId]);
-    return comments.length
+  const comments = await get("/comments", ["postId", postId]);
+  return comments.length;
 }
 
 async function countWords(string, limit = 10) {
-    const words = string.split(/[ \n]/);
-    const occurences = new Set(words);
-    const result = new Map;
-    //console.log(occurences)
-    for (const occurence of occurences) {
-        let count = 0;
-        for (const word of words) {
-            if(word === occurence) {
-                count++;
-            }
-        }
-        result.set(occurence, count)
+  const words = string.split(/[ \n]/);
+  const occurences = new Set(words);
+  const result = new Map();
+  //console.log(occurences)
+  for (const occurence of occurences) {
+    let count = 0;
+    for (const word of words) {
+      if (word === occurence) {
+        count++;
+      }
     }
-    //console.log(result);
-    const sorted = new Map([...result.entries()].sort((a, b) => b[1] - a[1]));
-    //console.log(sorted)
-    const iterator = sorted.entries();
-    const topTenSorted = new Map;
-    for (let i = 0; i < limit; i++) {
-        const set = iterator.next().value;
-        const key = set[0];
-        const value = set[1];
-        topTenSorted.set(key, value)
-    }
-    return topTenSorted;
+    result.set(occurence, count);
+  }
+  //console.log(result);
+  const sorted = new Map([...result.entries()].sort((a, b) => b[1] - a[1]));
+  //console.log(sorted)
+  const iterator = sorted.entries();
+  const topTenSorted = new Map();
+  for (let i = 0; i < limit; i++) {
+    const set = iterator.next().value;
+    const key = set[0];
+    const value = set[1];
+    topTenSorted.set(key, value);
+  }
+  return topTenSorted;
 }
 
 function topWords(words) {
-    console.log(words.size)
-    let string = "<p>Top 10 des mots les plus utilisés dans les posts</p>";
-    const iterator = words.entries();
-    for (let i = 0; i < words.size; i++) {
-        const word = iterator.next().value;
-        string += `<p>mot: ${word[0]} - occurences: ${word[1]}</p>`
-    }
-    return string;
+  console.log(words.size);
+  let string = "<p>Top 10 des mots les plus utilisés dans les posts</p>";
+  const iterator = words.entries();
+  for (let i = 0; i < words.size; i++) {
+    const word = iterator.next().value;
+    string += `<p>mot: ${word[0]} - occurences: ${word[1]}</p>`;
+  }
+  return string;
 }
 
-userSelect.addEventListener("click", ()=> {
-    if(userSelect.value !== currentUserId) {
-        currentUserId = userSelect.value;
-        if(postsContainer.children.length > 0){
-            const currentPosts = postsContainer.children[2];
-            postsContainer.removeChild(currentPosts);
-        }
-        const userId = userSelect.value;
-        displayPostsUser(userId);
+userSelect.addEventListener("click", () => {
+  if (userSelect.value !== currentUserId) {
+    currentUserId = userSelect.value;
+    if (postsContainer.children.length > 0) {
+      const currentPosts = postsContainer.children[2];
+      postsContainer.removeChild(currentPosts);
     }
-})
+    const userId = userSelect.value;
+    displayPostsUser(userId);
+  }
+});
